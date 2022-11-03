c6eed35120ecef345245e918c961e9135a2c2653
Jack Stanek <stanekjack@gmail.com>
2022-11-02 20:15:45 -0500

    Updates

---
+++ precache-manifest.1f90f834dc980271faf22d115e122497.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a736945bde49bd9c1a97",
+    "url": "/saint-paul-capital-improvements/static/js/main.84cb654f.chunk.js"
+  },
+  {
+    "revision": "4152b587e04105299a0b",
+    "url": "/saint-paul-capital-improvements/static/js/runtime~main.5aaf99e9.js"
+  },
+  {
+    "revision": "fdd506451c404d176a51",
+    "url": "/saint-paul-capital-improvements/static/js/2.32841c5f.chunk.js"
+  },
+  {
+    "revision": "174e276a00ef495c0eccd90695c06f27",
+    "url": "/saint-paul-capital-improvements/index.html"
+  }
+];
